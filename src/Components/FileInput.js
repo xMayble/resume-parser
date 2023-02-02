643c9b489185144c89db844106deb3206b0603cc
Mehbub Rohit <mehburohit@gmail.com>
2023-02-01 19:53:20 -0500

    changes to log messages

--- src/Components/FileInput.js
+++ src/Components/FileInput.js
@@ -1,21 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './FileInput.css';
 
 function FileInput() {
     const [file, setFile] = useState(null);
+    const fileInputRef = React.useRef(null);
 
     const handleFileUpload = (event) => {
+        console.log('File upload event fired');
         setFile(event.target.files[0]);
         console.log('File Uploaded:', file.name);
     };
 
+    useEffect(() => {
+        if (file) {
+            console.log('File Uploaded:', file.name);
+        }
+    }, [file]);
+
+    const handleButtonClick = () => {
+        fileInputRef.current.click();
+    }
+
     return (
         <div className='file-input'>
-            <label htmlFor='file-upload'>
-                Upload Resume
-            </label>
-
+            <button onClick={handleButtonClick}>Upload Resume!</button>
             <input 
+                ref={fileInputRef}
                 id = "file-upload"
                 type = "file"
                 style = {{ display: "none" }}
